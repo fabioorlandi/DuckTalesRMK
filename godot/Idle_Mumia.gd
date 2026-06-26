8092a6f9708fbb65e35bf47511f55d44c556f685
julioFSch <julio.scheuermann@gmail.com>
2026-06-25 21:57:44 -0300

    Assets novos Inimigos

---
+++ godot/Idle_Mumia.gd
@@ -0,0 +1,29 @@
+extends State
+class_name IDLE_MUMIA
+
+@export var animation = &"idle"
+@export var reactivate_delay := 1.0
+var actor
+var timer := 0.0
+
+func _ready():
+	actor = get_parent().get_parent()
+	
+func enter() -> void:
+	timer = 0.0
+	actor.velocity = Vector2.ZERO
+	
+	var anim = actor.get_node("AnimatedSprite2D")
+	anim.play(animation)
+
+func update(delta: float) -> void:
+	timer += delta
+	
+	if actor.player_on_screen and timer >= reactivate_delay:
+		transitioned.emit(self, "ativado")
+
+func physics_update(_delta: float) -> void:
+	pass
+
+func exit() -> void:
+	pass
