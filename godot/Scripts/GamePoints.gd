3ce61591f4eb0702e9424e17ca9d8e62b0563e7b
William Rizzi <william.rizzi@hotmail.com>
2026-07-11 04:04:00 -0300

    Game Scenes

--- godot/Scripts/GamePoints.gd
+++ godot/Scripts/GamePoints.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var actualDifficulty: String
+
 @export var totalScore: int
 
 @export var health: int
@@ -7,3 +9,10 @@ extends Node
 
 @export var lifes: int
 @export var lifesCap: int
+
+func ResetGamepoints() -> void:
+	totalScore = 0
+	health = 6
+	healthCap = 6
+	lifes = 2
+	lifesCap = 2
