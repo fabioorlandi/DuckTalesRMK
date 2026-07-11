73b64d2dcd4f5ea700cf5ba23eaac630bb2aba4c
Fábio <programacao.acronym127@passinbox.com>
2026-07-11 11:50:45 -0300

    Ajuste espinhos

--- godot/Scripts/espinhos.gd
+++ godot/Scripts/espinhos.gd
@@ -1,23 +1,18 @@
 extends Area2D
 var isOnThorns: bool = false
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if isOnThorns:
 		var body = get_tree().get_nodes_in_group("Patinhas")[0]
 
-		var currentState = body.get_node("FSM").current_state
-		currentState.transitioned.emit(currentState, "damage")
+		if not body.onPogo and body.invulnerability_ticks == 0:
+			var currentState = body.get_node("FSM").current_state
+			currentState.transitioned.emit(currentState, "damage")
 
 func _on_body_entered(body: Node2D) -> void:
-	if not isOnThorns\
-		and body.name == "Patinhas"\
-		and body.invulnerability_ticks == 0\
-		and not body.onPogo:
+	if body.name == "Patinhas":
 		isOnThorns = true
-		var currentState = body.get_node("FSM").current_state
-		currentState.transitioned.emit(currentState, "damage")
-
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "Patinhas" and not body.onPogo:
+	if body.name == "Patinhas":
 		isOnThorns = false
